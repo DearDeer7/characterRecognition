00db015c8ce5a27e920ead9947c02e1a66d8b934
deardeer <deardeer0091@163.com>
2021-06-16 16:07:36 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/characterRecognition.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/characterRecognition.dir/main.cpp.obj"
+  "characterRecognition.exe"
+  "characterRecognition.exe.manifest"
+  "characterRecognition.pdb"
+  "libcharacterRecognition.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/characterRecognition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,192 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// created a directory named files,
+// then copy your files path into ADDR string below
+// remember to add "\\" in the end
+const string ADDR = "C:\\Users\\14795\\Desktop\\CR\\files\\";
+
+// crop matrix
+// not used yet!
+vector<string> crop(vector<string>& vs) {
+    int l = INT_MAX;
+    int r = INT_MIN;
+    int u = INT_MAX;
+    int d = INT_MIN;
+    for(int i = 0; i < vs.size(); ++i) {
+        for(int j = 0; j < vs[0].size(); ++j) {
+            char& c = vs[i][j];
+            if(c == '1') {
+                l = min(l, j);
+                r = max(r, j);
+                u = min(u, i);
+                d = max(d, i);
+            }
+        }
+    }
+    cout << l << " " << r << " " << u << " " << d << endl;
+    int len = max(r - l + 1, d - u + 1);
+    r = l + len;
+    d = u + len;
+    cout << l << " " << r << " " << u << " " << d << endl;
+    vector<string> ans;
+    for(int i = 0; i < vs.size(); ++i) {
+        if(i < u || i > d) continue;
+        string cur;
+        for(int j = 0; j < vs[0].size(); ++j) {
+            if(j < l || j > r) continue;
+            cur += vs[i][j];
+        }
+        ans.push_back(cur);
+    }
+    return ans;
+}
+
+// get character matrix by ifstream
+vector<string> getMatrix(ifstream& in) {
+    vector<string> vs;
+    string s;
+    while(getline(in, s)) {
+        vs.push_back(s);
+    }
+    return vs;
+}
+
+// get subMatrix by given r and c (starting index)
+vector<string> getSub(vector<string>& vs, int r, int c) {
+    vector<string> sub;
+    for(int i = 0; i <= 149; ++i) {
+        string cur;
+        for(int j = 0; j <= 149; ++j) {
+            cur.push_back(vs[i + r][j + c]);
+        }
+        sub.push_back(cur);
+    }
+    return sub;
+}
+
+// get character pixel ratio
+double getRatio(vector<string>& vs) {
+    int one = 0;
+    for(auto s : vs) {
+        for(char c : s) {
+            one += c == '1';
+        }
+    }
+    if(one == 0){
+        cout << "No pixel found!" << endl;
+        exit(0);
+    }
+    return one * 1.0 / 90000.0;
+}
+
+vector<double> getRatios(vector<string>& vs) {
+    vector<double> ans;
+    vector<string> sub = getSub(vs, 0, 0);
+    ans.push_back(getRatio(sub));
+
+    sub = getSub(vs, 0, 150);
+    ans.push_back(getRatio(sub));
+
+    sub = getSub(vs, 150, 0);
+    ans.push_back(getRatio(sub));
+
+    sub = getSub(vs, 150, 150);
+    ans.push_back(getRatio(sub));
+
+    return ans;
+}
+
+// calc match ratios and print results
+void calc(const vector<vector<double>>& ratios,const vector<double>& myRatios) {
+    vector<double> res(5);
+    double mi = 1.0, idx = -1;
+    for(int i = 0; i < 5; ++i) {
+        double t = 0.0;
+        for(int j = 0; j < 4; ++j) {
+            t += fabs(ratios[i][j] - myRatios[j]);
+        }
+        res[i] = t / 4;
+        if(mi > res[i]) {
+            mi = res[i];
+            idx = i;
+        }
+        cout << res[i] << " ";
+    }
+    cout << endl;
+    double percent = 1.0 - res[idx];
+    cout << "Best Match = " << idx + 1 << " with ratio " << percent * 100.0 << "%" << endl;
+}
+
+void illegalInput() {
+    cout << "illegal input!" << endl;
+    exit(0);
+}
+
+int main() {
+
+    ofstream out(ADDR + "out.txt");
+    vector<vector<double>> ratios;
+    for(int i = 1; i <= 5; ++i) {
+        ifstream in(ADDR + to_string(i) + ".txt");
+        vector<string> M = getMatrix(in);
+        ratios.push_back(getRatios(M));
+        in.close();
+    }
+
+    int option = -1;
+    cout << "Please input option:" << endl;
+    cin >> option;
+    if(option == 1) {
+        cout << "----Mode1----" << endl;
+        string txt;
+        cout << "Please input full txt file name:" << endl;
+        cin >> txt;
+        if(txt.size() < 5 || txt.find(".txt") == string::npos) {
+            illegalInput();
+        }
+        ifstream in(ADDR + txt);
+        vector<string> M = getMatrix(in);
+        vector<double> myRatios = getRatios(M);
+        in.close();
+
+        calc(ratios, myRatios);
+    }
+    else if(option == 2) {
+        cout << "----Mode2----" << endl;
+        string txt;
+        cout << "Please input full txt file name:" << endl;
+        cin >> txt;
+        if(txt.size() < 5 || txt.find(".txt") == string::npos) {
+            illegalInput();
+        }
+        ifstream in(ADDR + txt);
+        vector<string> M = getMatrix(in);
+        vector<double> myRatios = getRatios(M);
+        in.close();
+
+        calc(ratios, myRatios);
+    }
+    else if(option == 3) {
+        cout << "----Mode3----" << endl;
+        string txt;
+        cout << "Please input txt full file name:" << endl;
+        cin >> txt;
+        if(txt.size() < 5 || txt.find(".txt") == string::npos) {
+            illegalInput();
+        }
+        ifstream in(ADDR + txt);
+        vector<string> M = getMatrix(in);
+        vector<double> myRatios = getRatios(M);
+        in.close();
+
+        calc(ratios, myRatios);
+    }
+    else {
+
+    }
+
+    out.close();
+
+    return 0;
+}
